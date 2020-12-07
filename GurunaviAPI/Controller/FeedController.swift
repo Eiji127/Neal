48b55b7a954427e8ed4ec1eefb625dd49c60b4f4
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-12-07 21:04:01 +0900

    [UPDATE]: add_some_files

--- GurunaviAPI/Controller/FeedController.swift
+++ GurunaviAPI/Controller/FeedController.swift
@@ -5,4 +5,57 @@
 //  Created by 白数叡司 on 2020/12/07.
 //
 
-import Foundation
+import UIKit
+
+
+private let reuseIdentifier = "ShopInfoCell"
+
+class FetchController: UICollectionViewController {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - API
+    
+    
+    // MARK: - Helper
+    
+    func configureUI() {
+        view.backgroundColor = .red
+        
+        
+    }
+
+}
+
+// MARK: - UICollectionViewDelegate/DataSource
+
+extension FetchController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension FetchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        <#code#>
+    }
+}
--- GurunaviAPI/View/ShopInfoCell.swift
+++ GurunaviAPI/View/ShopInfoCell.swift
@@ -5,4 +5,35 @@
 //  Created by 白数叡司 on 2020/12/07.
 //
 
-import Foundation
+import UIKit
+
+class ShopInfoCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.text = "Shop Name"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .blue
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Helpers
+    
+}
